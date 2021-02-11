0aee3d6e050d304d5e1528be410c541b1ea8b1c8
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-02-11 11:36:12 +0100

    First commit

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type:String,
+    required:true
+  },
+  lastname: {
+    type:String,
+    required:true
+  },
+  password: {
+    type: String,
+    requred: true,
+  },
+  city: {
+    type:String,
+    required:true
+  }
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("flatme JS imported successfully!");
+  },
+  false
+);
