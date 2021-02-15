a0375ba096fd06cb456913991a618a83fa3980b2
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-02-15 12:33:16 +0100

    Test map axios

--- public/js/script.js
+++ public/js/script.js
@@ -11,11 +11,17 @@ document.addEventListener(
 let map = L.map('map').setView([40.341705, -17.968551], 1.2)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
 
-async function getReviews() {
-  const res = await fetch('/flatme/comments')
-}
+// async function getReviews() {
+//   const res = await fetch('/flatme/comments')
+// }
 
-getReviews()
+// getReviews()
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/flatme";
+
+axios.get(MONGO_URI/comments)
+  .then((data) => console.log(data))
+
 
 module.exports = map
 
