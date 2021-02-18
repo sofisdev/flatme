3a8f7ab6a168acbffe0bcea4a238ff9566f9ccdf
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-02-18 12:15:08 +0100

    regex password update

--- routes/index.js
+++ routes/index.js
@@ -46,9 +46,9 @@ router.post('/signup', (req, res, next) => {
   }
 
   //check for password
-  let regexPass = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
+  let regexPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#\$%\^&\*]).{8,20}$/;
   if (!regexPass.test(password)) {
-     res.render('signup.hbs', {msg: 'Password needs to have special characters, at least one Upppercase letter and one number and be 8 characters at least', profileBody})
+     res.render('signup.hbs', {msg: 'Password needs to have at least one Upppercase letter, one number, one special character and be 8 characters long at least', profileBody})
      return;
   }
 
